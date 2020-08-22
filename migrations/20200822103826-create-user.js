b22c9324d040992070aa000235769069e72a751e
adimarbun <adigunawan1405@gmail.com>
2020-08-22 18:13:11 +0700

    install ORM Sequelize

---
+++ migrations/20200822103826-create-user.js
@@ -0,0 +1,35 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      fullname: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      about: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Users");
+  },
+};
